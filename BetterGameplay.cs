b826289109cd0d1b2f1bc8ef3bc8f2f9ce9a570f
XoXFaby <xoxfaby@gmail.com>
2020-09-28 07:53:16 +0200

    Initial Commit

---
+++ BetterGameplay.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using BepInEx;
+using RoR2;
+using R2API.Utils;
+using UnityEngine;
+using System.Collections;
+
+namespace BetterGameplay
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.xoxfaby.BetterGameplay", "BetterGameplay", "1.0.0")]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    public class BetterGameplay : BaseUnityPlugin
+    {
+        char[] collisionMap = "11111111100010000000110011001000".ToCharArray();
+                                         
+        public void Awake()
+        {
+            for(int i = 0; i < 32; i++)
+            {
+                Physics.IgnoreLayerCollision(22, i, collisionMap[31 - i] == '0' ? true : false) ;
+            }
+            On.RoR2.PickupDropletController.Start += (orig, self) =>
+            {
+                orig(self);
+                self.gameObject.layer = 22;
+            };
+            On.RoR2.MapZone.TryZoneStart += (orig, self, collider) => {
+                if (self.zoneType == MapZone.ZoneType.OutOfBounds)
+                {
+                    if (collider.GetComponent<PickupDropletController>() || collider.GetComponent<GenericPickupController>())
+                    {
+                        SpawnCard spawnCard = ScriptableObject.CreateInstance<SpawnCard>();
+                        spawnCard.hullSize = HullClassification.Human;
+                        spawnCard.nodeGraphType = RoR2.Navigation.MapNodeGroup.GraphType.Ground;
+                        spawnCard.prefab = Resources.Load<GameObject>("SpawnCards/HelperPrefab");
+                        GameObject gameObject = DirectorCore.instance.TrySpawnObject(new DirectorSpawnRequest(spawnCard, new DirectorPlacementRule
+                        {
+                            placementMode = DirectorPlacementRule.PlacementMode.NearestNode,
+                            position = collider.transform.position
+                        }, RoR2Application.rng));
+                        if (gameObject)
+                        {
+                            Debug.Log("TP Item Back");
+                            TeleportHelper.TeleportGameObject(collider.gameObject, gameObject.transform.position);
+                            UnityEngine.Object.Destroy(gameObject);
+                        }
+                        UnityEngine.Object.Destroy(spawnCard);
+                    }
+                }
+
+                orig(self, collider);
+            };
+
+            On.RoR2.EquipmentSlot.ExecuteIfReady += (orig, self) =>
+            {
+                if((self.inventory != null) ? self.inventory.GetItemCount(ItemIndex.AutoCastEquipment) > 0 : false)
+                {
+                    if(SceneInfo.instance.sceneDef.nameToken == "MAP_BAZAAR_TITLE")
+                    {
+                        return false;
+                    }
+                }
+                return orig(self);
+            };
+
+            On.RoR2.Inventory.UpdateEquipment += (orig, self) =>
+            {
+
+                if (self.GetItemCount(ItemIndex.AutoCastEquipment) > 0)
+                {
+                    if (SceneInfo.instance.sceneDef.nameToken == "MAP_BAZAAR_TITLE")
+                    {
+                        for(int i = 0; i < self.equipmentStateSlots.Length; i++)
+                        {
+                            self.equipmentStateSlots[0].chargeFinishTime.t += Time.deltaTime;
+                        }
+                        return;
+                    }
+                }
+                orig(self);
+            };
+        }
+    }
+}
---
+++ Thunderstore/README.md
@@ -0,0 +1,33 @@
+# BetterGameplay
+
+A simple mod that adds various QOL improvements.  
+
+#### THIS MOD IS IN EARLY DEVELOPMENT AND MAY HAVE BUGS
+
+I highly recommend that when using this mod ( or any mod ) you understand that bugs can happen.
+
+
+## Currently implemented:
+- Items that fall off the map are teleported up
+- Your equipment is paused in the Baazar if you have a Gesture of the Drowned
+
+## Support Me
+
+If you like what I'm doing, consider supporting me through GitHub Sponsors so I can keep doing it:
+
+https://github.com/sponsors/xoxfaby
+
+## Help & Feedback
+
+If you need help or have suggestions, create an issue on github, join my discord or find me on the RoR2 Modding Discord 
+
+[My Discord](https://discord.gg/Zy2HSB4) XoXFaby#1337
+
+https://github.com/xoxfaby/BetterUI
+
+If you have suggestions for more QoL improvements, feel free to contact me!
+
+## Changelog
+
+### v1.0.0 
+ - Initial Release
